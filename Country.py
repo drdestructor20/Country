aaa6ab969c183893004371f7f868771bfaa2c79b
username <useremail>
2024-04-29 14:54:44 -0700

    Final Code

--- Country.py
+++ Country.py
@@ -2,12 +2,8 @@
 #CIS261
 #Country
 
-from stat import UF_APPEND
-from tkinter import N
-
-
 def display_menu():
-    print("Welcome To The Country Code Manager\n")
+    print("Welcome To The Country Code Manager!\n")
     print("1. Display A Country")
     print("2. Add A Country")
     print("3. Delete A Country")
@@ -16,7 +12,7 @@ def display_menu():
 def display_country_codes(countries):
     codes = list(countries.keys())
     codes.sort()
-    codes_line =  "\nCountry Codes: "
+    codes_line = "\nCountry Codes: "
     for code in codes:
         codes_line += code + " "
     print(codes_line)
@@ -28,44 +24,43 @@ def display_country_details(countries):
         name = countries[code]
         print(f"\nCountry Name: {name}.\n")
     else:
-        print("\nThere Is No Country With That COde.\n")
+        print("\nThere Is No Country With That Code.\n")
         
 def add_country(countries):
     code = input("\nPlease Enter The Country Code: ").upper() 
-    if code in countries: 
-        name = countries[code]
-        print(f"\n{name} Is Already Using This Code.\n")
-    else:
-        name = input("\nEnter The Country Name: ").title() 
-        countries[code] = name 
-        print(f"\nThe Country {name} With Code {code} Was Added Successfully.\n")
-              
-def delete_country(countries):
-    code = inpute("\nEnter A Country Code To Delete: ").upper() 
     if code in countries:
-        name = countries.pop(code)
-        print(f"\nThe Country With Code {code} has been deleted.\n")
+        name = countries[code]
+        print(f"\n{name} Is Already Using Code.\n")
     else:
-        print("\nInvalid Code. Please Try Again.\n")
-    
+        name = input("\nEnter The Country Name: ").title()
+        countries[code] = name
+        print(f"\nThe Country {name} With Code {code} Was Added Successfully.\n") 
+        
+def delete_country(countries):
+    code = input("\nEnter The Country Code To Delete: ").upper() 
+    if code in countries:
+        name = countries.pop(code) 
+        print(f"\nThe Country With Code {code} Has Been Deleted.\n")
+    else:
+        print("Invalid Code, Please Try Again.\n")
+        
 def main():
     countries = {"MX": "Mexico", "US": "United States", "ES": "Spain", "IE": "Ireland"}
     
     while True:
         display_menu()
-        choice = input("\nPlease Enter Your Choice (1-4): ")
-        if choice == '1':
+        selection = input("\nEnter Your Selection (1-4): ")
+        if selection == '1':
             display_country_details(countries)
-        elif choice == '2':
+        elif selection == '2':
             add_country(countries)
-        elif choice == '3':
-            delete_country(countires)
-        elif choice == '4':
-            print("\nThank You For Using The Country Code Manager! ")
+        elif selection == '3':
+            delete_country(countries)
+        elif selection == '4':
+            print("\nThank You For Using The Country Code Manager!")
             break
         else:
-            print("\nInvalid Command, Please try again.\n")
-            
-if __name__ == " __main__":
+            print("\nInvalid Command, Please Try Again.\n")
+
+if __name__ == "__main__":
     main()
-    
